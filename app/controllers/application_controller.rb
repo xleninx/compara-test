c660682e1b2992abc4c5165ec66b7c98ba4f15cd
Lenin Luque <xleninx@gmail.com>
2017-06-11 21:35:56 -0400

    add token generator

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  
+  private
+    def generate_token
+      token = DealerConnectionService.new.generate_token
+      if token.present?
+        session[:token] = token
+        session[:valid_token] = true
+      else
+        generate_token
+      end
+    end
 end
