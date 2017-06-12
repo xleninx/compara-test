c5d3cea39e87bf3ab5f75f82841c2961b8d531e4
Lenin Luque <xleninx@gmail.com>
2017-06-11 21:36:40 -0400

    add helper for cards

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+
+  def suit_char(suit_name)
+    suit_hash[suit_name.to_sym]
+  end
+
+  def suit_hash
+    { 
+      'spades': '♠',
+      'diamonds': '♦',
+      'hearts': '♥',
+      'clubs': '♣'
+    }
+  end
 end
